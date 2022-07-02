7ee088d0246c3384a5b567f53780c299533b6d8e
ROHIT KUMAR <97289743+rohitgupta8055@users.noreply.github.com>
2022-07-02 13:42:54 +0530

    comit
    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./My Components/Header";
+import Content from './My Components/Content';
+import Footer from './My Components/Footer'
+
+
+function App() {
+  return (<>
+    <Header title="nOc"/>
+     <Content />
+     <Footer/>
+     </>
+  );
+}
+
+export default App;
---
+++ src/My Components/Content.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function Content() {
+  return (
+    
+        <div class="p-3 mb-2 bg-black text-white">
+            <div>
+      <p class="font-monospace text-center my-3 fs-5">Decarbonizing Industries</p>
+      <p class="font-monospace text-center my-3 fs-6">To take our world to net zero carbon Emission</p>
+       </div>
+       <div class="">
+     <p class="font-monospace text-center my-3 fs-5">Net Zero Carbon Future For Multiple industries</p>
+    <ul >
+  <li class="list-group-item">Power Plants(coal powered)</li>
+  <li class="list-group-item">Steel Plants</li>
+  <li class="list-group-item">Cement Plants</li>
+  <li class="list-group-item">Ships</li>
+  <li class="list-group-item">Heavy Duty Trucks</li>
+  <li class="list-group-item">Others</li>
+    </ul>
+    </div>
+   
+   <div>
+   <p class="font-monospace text-center my-3 fs-5">Benefits<br/>nOc v/s other methods</p>
+   <ul >
+  <li class="list-group-item">Efficient</li>
+  <li class="list-group-item">Lower Cost</li>
+  <li class="list-group-item">Works At Any Co2 Concentration</li>
+  <li class="list-group-item">Small Size</li>
+  <li class="list-group-item">Scalable</li>
+    </ul>
+   </div>
+
+
+   <div class="mt-1 bg-black text-white text-center">
+   <a style={{color:"white",textDecoration:"none"}} href="https://forms.gle/puAfXMbsy24iTRAp9">Contact Us</a>
+      <br/>Terms &emsp; Privacy <br/>© nOc 2022
+    </div>
+   
+
+    </div>
+      
+  )
+}
+
+export default Content
